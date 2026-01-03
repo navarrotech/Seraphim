0af429c5fe84316f9c3e4a7beb46c9c5721c6be4
Alex Navarro <alex@navarrotech.net>
2026-01-03 16:09:11 -0700

    v2 First Overhauls

---
+++ eslint.config.ts
@@ -0,0 +1,23 @@
+// Copyright © 2026 Jalapeno Labs
+
+import { defineConfig, globalIgnores } from 'eslint/config'
+import cliBaseConfig from '@jalapenolabs/cli/eslint'
+
+export default defineConfig([{
+  extends: [ cliBaseConfig ],
+  },
+  globalIgnores([
+    '.yarn/**',
+    '**/vite-env.d.ts',
+  ]),
+  {
+  rules: {
+    'react-hooks/exhaustive-deps': 'off',
+    'license-header/header': [
+      'error',
+      [
+        `// Copyright © ${new Date().getFullYear()} Jalapeno Labs`,
+      ],
+    ],
+  },
+}])
